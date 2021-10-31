30258fd06f0383e19e2bbfab819b4ae97776bc6e
Rufus-Sam <68532159+Rufus-Sam@users.noreply.github.com>
2021-10-31 19:52:57 +0530

    small change

--- frontend/src/GroupPage/GroupPage.js
+++ frontend/src/GroupPage/GroupPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { Form, Button } from 'react-bootstrap'
 import { connectWithWebSocket } from '../utils/wssConnection/wssConnection'
 
